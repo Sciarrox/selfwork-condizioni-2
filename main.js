aa5cb70b6708cd891b0c40ee96c30da6970ee5f1
Sciarrox <sciarrox@gmail.com>
2025-01-13 23:37:25 +0100

    selfwork modificato condizioni2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 //     console.log('non ci sono piu le mezze stagioni');
 // }else if(temperatura >= 30){
 //     console.log('lu mare, lu sole, lu ientu');
-// }else  if(temperatura < 30 && temperatura > 20){
+// }else  if(temperatura < 30 && temperatura >= 20){
 //     console.log('mi dia una peroni sudata');
 // }else if(temperatura < 0 && temperatura > -10){
 //     console.log('non e’ tanto il freddo quanto l’umidità');
@@ -16,22 +16,22 @@
 
 let temperatura = prompt('Inserisci qui la temperatura')
 
-switch(true){
+switch (true) {
     case (temperatura < 20 && temperatura > 0):
-        console.log('non ci sono piu le mezze stagioni' );
-    break;
+        console.log('non ci sono piu le mezze stagioni');
+        break;
 
     case (temperatura >= 30):
-    console.log('lu mare, lu sole, lu ientu');
-    break;
+        console.log('lu mare, lu sole, lu ientu');
+        break;
 
-    case (temperatura < 30 && temperatura >20):
-    console.log('mi dia una peroni sudata');
-    break;
+    case (temperatura < 30 && temperatura >= 20):
+        console.log('mi dia una peroni sudata');
+        break;
 
     case (temperatura < 0 && temperatura > -10):
-    console.log('non e’ tanto il freddo quanto l’umidità');
-    break;
+        console.log('non e’ tanto il freddo quanto l’umidità');
+        break;
 
     case (temperatura < -10):
         console.log('copriti…ancora ti raffreddi');
